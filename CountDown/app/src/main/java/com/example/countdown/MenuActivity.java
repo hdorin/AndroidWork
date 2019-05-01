0fbb8c68ee95390d5cd3a2972a494c2e6f9c46cf
Dorin Haloca <halocadorin@outlook.com>
2019-05-01 22:54:11 +0300

    Gata si social share

--- CountDown/app/src/main/java/com/example/countdown/MenuActivity.java
+++ CountDown/app/src/main/java/com/example/countdown/MenuActivity.java
@@ -1,9 +1,12 @@
 package com.example.countdown;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,5 +27,22 @@ public class MenuActivity extends AppCompatActivity {
     public void showHighScore(View view){
         Intent intent = new Intent(this, HighScoreActivity.class);
         startActivity(intent);
+    }
+    public void socialMediaShare(View view){
+        SharedPreferences sharedPref = this.getSharedPreferences("score", Context.MODE_PRIVATE);
+        int score = sharedPref.getInt("score", 0);
+        String message;
+        if(score==0) {
+            message = "I didn't score any points in CountDown!";
+        }else if(score==1){
+            message = "I scored " + score + " point in CountDown!";
+        }else{
+            message = "I scored " + score + " points in CountDown!";
+        }
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_TEXT, message);
+
+        startActivity(Intent.createChooser(share, "Share you highscore!"));
     }
 }
